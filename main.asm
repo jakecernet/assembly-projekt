e020f40ffa7de917e39e9c467171c4d5dc81388b
jakecernet <jaka.cernetic@students.vegova.si>
2023-12-21 21:08:13 +0100

    neki sm probu

--- main.asm
+++ main.asm
@@ -1,6 +1,4 @@
-;   Display svetlosti na 7 segmentnem zaslonu   ;
-
-.org 0x0034 ;   vključimo zaslon   ;
+.org 0x0134 ;   vključimo zaslon   ;
 
 .include "display.asm" ;   program za prikaz na 7 segmentnem zaslonu   ;
 .include "photoresistor.asm" ;   program za branje vrednosti fotorezistorja   ;
@@ -8,10 +6,85 @@
 .org 0x0000 ;   začetek programa   ;
 
 setup:
-            call SetupPhotoresistor ;   nastavitev fotorezistorja   ;
-            call SetupDisplay ;   nastavitev 7 segmentnega zaslona   ;
-            rjmp loop ;   skočimo na loop   ;
+    call SetupPhotoresistor ;   nastavitev fotorezistorja   ;
+    call SetupDisplay ;   nastavitev 7 segmentnega zaslona   ;
+    rjmp loop ;   skočimo na loop   ;
 
 loop:
-            call ReadLDR ;   preberemo vrednost fotorezistorja   ;
-            jmp loop ;   skok na začetek programa   ;
+    call ReadLDR ;   preberemo vrednost fotorezistorja   ;
+    call ComputeDisplay ;   izračunamo vrednost za prikaz   ;
+    jmp loop ;   skok na začetek programa   ;
+
+ComputeDisplay:
+    ; Check the brightness level and set the displayed number accordingly
+    ; The threshold values are adjusted based on your example (0 for zero, 100 for nine)
+
+    cpi r16, 0 ;   If r16 is equal to or less than 0, display 0   ;
+    breq display0 ;   skočimo na displayZero   ;
+
+    cpi r16, 10 ;   If r16 is less than 10, display 1   ;
+    brlo display1 ;
+
+    cpi r16, 20 ;   If r16 is less than 20, display 2   ;
+    brlo display2 ;
+
+    cpi r16, 30 ;   If r16 is less than 30, display 3   ;
+    brlo display3 ;
+
+    cpi r16, 40 ;   If r16 is less than 40, display 4   ;
+    brlo display4 ;
+
+    cpi r16, 50 ;   If r16 is less than 50, display 5   ;
+    brlo display5 ;
+
+    cpi r16, 60 ;   If r16 is less than 60, display 6   ;
+    brlo display6 ;
+
+    cpi r16, 70 ;   If r16 is less than 70, display 7   ;
+    brlo display7 ;
+
+    cpi r16, 80 ;   If r16 is less than 80, display 8   ;
+    brlo display8 ;
+
+    ; If r16 is greater than or equal to 80, display 9
+    rjmp display9 ;
+
+display0:
+    call displayZero ;   prikažemo 0   ;
+    ret ;   vrnemo se na loop   ;
+
+display1:
+    call displayOne ;   prikažemo 1   ;
+    ret ;   vrnemo se na loop   ;
+
+display2:
+    call displayTwo ;   prikažemo 2   ;
+    ret ;   vrnemo se na loop   ;
+
+display3:
+    call displayThree ;   prikažemo 3   ;
+    ret ;   vrnemo se na loop   ;
+
+display4:
+    call displayFour ;   prikažemo 4   ;
+    ret ;   vrnemo se na loop   ;
+
+display5:
+    call displayFive ;   prikažemo 5   ;
+    ret ;   vrnemo se na loop   ;
+
+display6:
+    call displaySix ;   prikažemo 6   ;
+    ret ;   vrnemo se na loop   ;
+
+display7:
+    call displaySeven ;   prikažemo 7   ;
+    ret ;   vrnemo se na loop   ;
+
+display8:
+    call displayEight ;   prikažemo 8   ;
+    ret ;   vrnemo se na loop   ;
+
+display9:
+    call displayNine ;   prikažemo 9   ;
+    ret ;   vrnemo se na loop   ;
--- photoresistor.asm
+++ photoresistor.asm
@@ -20,5 +20,58 @@ SetupPhotoresistor:
 
 ReadLDR:
                 ;   Začnemo branje   ;
-                sbi ADCSRA, ADSC ;   Začnemo branje   ;
-                
+                ldi r16, (1 << ADEN) | (1 << ADSC) ;   Nastavimo ADSC in ADEN   ;
+                sts ADCSRA, r16
+
+                ;   Počakamo na rezultat   ;
+                ldi r16, (1 << ADIF) ;   Nastavimo ADIF   ;
+                cpi r16, 0 ;   Preverimo, če je ADIF 0   ;
+                breq ReadLDR ;   Če je 0, ponovno poženemo branje   ;
+
+                ;   Preberemo rezultat   ;
+                lds r16, ADCL ;   Preberemo ADCL   ;
+                lds r17, ADCH ;   Preberemo ADCH   ;
+
+                ;   Shranimo rezultat deljen z 10 v r16, funkcija div ne obstaja   ;
+                ldi r18, 10 ;   Delimo z 10   ;
+                mov r19, r17 ;   Shranimo ADCH v r19   ;
+                mov r20, r16 ;   Shranimo ADCL v r20   ;
+                rcall div ;   Delimo   ;
+                mov r16, r22 ;   Shranimo rezultat v r16   ;
+
+                ;   Vrnemo se nazaj   ;
+                ret
+
+;   Podprogram za deljenje   ;
+;   ; Delimo r19:r20 z r18   ;
+;   Rezultat shranimo v r22   ;
+
+div:
+                ;   Pripravimo registre   ;
+                clr r22 ;   Počistimo r22   ;
+                clr r23 ;   Počistimo r23   ;
+                clr r24 ;   Počistimo r24   ;
+                clr r25 ;   Počistimo r25   ;
+
+                ;   Preverimo, če je r19 >= r18   ;
+                cpi r19, 0 ;   Preverimo, če je r19 0   ;
+                breq divEnd ;   Če je 0, preskočimo   ;
+                cp r19, r18 ;   Preverimo, če je r19 >= r18   ;
+                brge divLoop ;   Če je >=, preskočimo   ;
+
+                ;   Preverimo, če je r19 < r18   ;
+                cp r19, r18 ;   Preverimo, če je r19 < r18   ;
+                brlt divEnd ;   Če je <, preskočimo   ;
+
+divLoop:
+                ;   Odštejemo r18 od r19   ;
+                sub r19, r18 ;   Odštejemo r18 od r19   ;
+                inc r22 ;   Povečamo r22 za 1   ;
+
+                ;   Preverimo, če je r19 >= r18   ;
+                cp r19, r18 ;   Preverimo, če je r19 >= r18   ;
+                brge divLoop ;   Če je >=, preskočimo   ;
+
+divEnd:
+                ;   Vrnemo se nazaj   ;
+                ret
