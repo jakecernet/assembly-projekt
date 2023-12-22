5e48dccaeac1e1ad00b5cecf65c1480b2753e42b
jakecernet <jaka.cernetic@students.vegova.si>
2023-12-22 15:16:19 +0100

    sdsd

--- photoresistor.asm
+++ photoresistor.asm
@@ -9,69 +9,43 @@ SetupPhotoresistor:
                 clr r16 ;   Počistimo r16   ;
 
                 ;   Nastavimo vhodni pin   ;
-                sbi DDRD, PD3 ;   Nastavimo pin PD3 kot vhod   ;
+                sbi DDRC, 0 ;   Nastavimo PC0 na izhod   ;
 
-                ;   Nastavimo ADC   ;
-                ldi r16, (1 << REFS0) ;   Nastavimo ADMUX na 0   ;
-                sts ADMUX, r16
+                ldi r20, 0xC0
+                sts ADMUX, r20 ;   Nastavimo ADMUX   ;
+
+                ;   Nastavimo ADCSRA   ;
+                ldi r20, 0x87 ;   Nastavimo ADEN, ADPS2, ADPS1, ADPS0   ;
+                sts ADCSRA, r20
 
                 ;   Vrnemo se nazaj   ;
                 ret
 
 ReadLDR:
                 ;   Začnemo branje   ;
-                ldi r16, (1 << ADEN) | (1 << ADSC) ;   Nastavimo ADSC in ADEN   ;
-                sts ADCSRA, r16
+                ldi r20, 0xC7 ;   Nastavimo ADSC, ADEN, ADPS2, ADPS1, ADPS0   ;
+                sts ADCSRA, r20
 
                 ;   Počakamo na rezultat   ;
-                ldi r16, (1 << ADIF) ;   Nastavimo ADIF   ;
-                cpi r16, 0 ;   Preverimo, če je ADIF 0   ;
-                breq ReadLDR ;   Če je 0, ponovno poženemo branje   ;
+                lds r20, ADCSRA ;   Preberemo ADCSRA   ;
+                sbrs r20, ADSC ;   Preverimo, če je ADSC 0   ;
+                rjmp ReadLDR ;   Če je 1, ponovimo   ;
 
                 ;   Preberemo rezultat   ;
                 lds r16, ADCL ;   Preberemo ADCL   ;
                 lds r17, ADCH ;   Preberemo ADCH   ;
 
-                ;   Shranimo rezultat deljen z 10 v r16, funkcija div ne obstaja   ;
-                ldi r18, 10 ;   Delimo z 10   ;
-                mov r19, r17 ;   Shranimo ADCH v r19   ;
-                mov r20, r16 ;   Shranimo ADCL v r20   ;
-                rcall div ;   Delimo   ;
-                mov r16, r22 ;   Shranimo rezultat v r16   ;
+                ;   Delimo z 10
+                ldi r18, 10 ;   Nastavimo r18 na 10   ;
+                call div ;   Delimo r19:r20 z r18   ;
 
                 ;   Vrnemo se nazaj   ;
                 ret
 
 ;   Podprogram za deljenje   ;
 ;   ; Delimo r19:r20 z r18   ;
-;   Rezultat shranimo v r22   ;
+;   Rezultat shranimo v r16  ;
 
 div:
-                ;   Pripravimo registre   ;
-                clr r22 ;   Počistimo r22   ;
-                clr r23 ;   Počistimo r23   ;
-                clr r24 ;   Počistimo r24   ;
-                clr r25 ;   Počistimo r25   ;
+            
 
-                ;   Preverimo, če je r19 >= r18   ;
-                cpi r19, 0 ;   Preverimo, če je r19 0   ;
-                breq divEnd ;   Če je 0, preskočimo   ;
-                cp r19, r18 ;   Preverimo, če je r19 >= r18   ;
-                brge divLoop ;   Če je >=, preskočimo   ;
-
-                ;   Preverimo, če je r19 < r18   ;
-                cp r19, r18 ;   Preverimo, če je r19 < r18   ;
-                brlt divEnd ;   Če je <, preskočimo   ;
-
-divLoop:
-                ;   Odštejemo r18 od r19   ;
-                sub r19, r18 ;   Odštejemo r18 od r19   ;
-                inc r22 ;   Povečamo r22 za 1   ;
-
-                ;   Preverimo, če je r19 >= r18   ;
-                cp r19, r18 ;   Preverimo, če je r19 >= r18   ;
-                brge divLoop ;   Če je >=, preskočimo   ;
-
-divEnd:
-                ;   Vrnemo se nazaj   ;
-                ret
